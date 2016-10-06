5aafd83e7f2932089eeee41dc8b552c6a7037297
gaspard <gaspard@gmail.com>
2016-10-06 11:39:19 +0200

    PlayingAnimation: a stateless component is enough

--- src/PlayingAnimation.js
+++ src/PlayingAnimation.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './PlayingAnimation.css';
 
-class PlayingAnimation extends Component {
-  render() {
-    return (
-      <div className="playing">
-        <div className="rect1"></div>
-        <div className="rect2"></div>
-        <div className="rect3"></div>
-        <div className="rect4"></div>
-        <div className="rect5"></div>
-      </div>
-    );
-  };
-}
+const PlayingAnimation = () => {
+  return (
+    <div className="playing">
+      <div className="rect1"></div>
+      <div className="rect2"></div>
+      <div className="rect3"></div>
+      <div className="rect4"></div>
+      <div className="rect5"></div>
+    </div>
+  );
+};
 
 export default PlayingAnimation;
