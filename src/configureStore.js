6b26db538cae725f09974b508512c3ce87113938
gaspard <gaspard@gmail.com>
2016-10-25 11:01:27 +0200

    redux-persist added

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,11 +1,13 @@
 import { createStore } from 'redux';
+import {persistStore, autoRehydrate} from 'redux-persist';
 import radioApp from './reducers/index';
 
 import preset from './data/preset.js';
 
 const configureStore = () => {
   const persistedState = preset;
-  let store = createStore(radioApp, persistedState);
+  let store = createStore(radioApp, persistedState, autoRehydrate());
+  persistStore(store, {whitelist: ['stations'], debounce: 1000, keyPrefix:'r'});
 
   // TODO: remove this hack
   window.store = store;
