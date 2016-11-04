f7eaecd223fe50d5bcae39331f2337815738648f
gaspard <gaspard@gmail.com>
2016-11-04 16:38:30 +0100

    volume is managed from input range

--- src/components/Player.js
+++ src/components/Player.js
@@ -7,7 +7,7 @@ import { playerPlay, playerPause } from '../actions';
 class Player extends Component {
   constructor(props) {
     super(props);
-    console.log('Player props',props);
+    // console.log('Player props',props);
     this.state =  {
       volume: props.volume, // TODO does this needs to be in props ? can't we use state ?
       buffer: 0,
@@ -17,9 +17,11 @@ class Player extends Component {
   }
   volume(ev) {
     this.setState({
-      volume: 10
+      volume: ev.target.value
     });
     console.log("volume", ev.target.value);
+    this.$audio.volume = ev.target.value / 100;
+
     ev.stopPropagation();
     ev.preventDefault();
   }
@@ -28,53 +30,59 @@ class Player extends Component {
     else this.props.dispatch(playerPause());
   }
   componentDidMount() {
+    // Media Events
     // https://developer.mozilla.org/en-US/docs/Web/Guide/Events/Media_events
-    let $audio = document.querySelector('audio');
+    this.$audio = document.querySelector('audio');
 
-    $audio.addEventListener('canplay', () => {
+    // an alternative would be using the webaudioapi
+    // http://codetheory.in/controlling-the-volume-of-an-audio-file-in-javascript/
+
+    this.$audio.onvolumechange = (ev) => {
+      console.log('volume has been changed to ', this.$audio.volume);
+    };
+
+    this.$audio.addEventListener('canplay', () => {
       console.log('loaded');
     });
-    $audio.onvolumechange = () => {
-      console.log('volume has been changed');
-    };
-    $audio.addEventListener('loadstart', () => {
+
+    this.$audio.addEventListener('loadstart', () => {
       this.setState({buffer: 10});
       console.log('loadstart');
     });
-    $audio.addEventListener('loadedmetadata', () => {
+    this.$audio.addEventListener('loadedmetadata', () => {
       console.log('loadedmetadata');
       this.setState({buffer: 20});
     });
-    $audio.addEventListener('loadeddata', () => {
+    this.$audio.addEventListener('loadeddata', () => {
       console.log('loadeddata');
       this.setState({buffer: 30});
     });
-    $audio.addEventListener('loaded', () => {
+    this.$audio.addEventListener('loaded', () => {
       console.log('loaded');
       this.setState({buffer: 50});
     });
-    $audio.addEventListener('canplaythrough', () => {
+    this.$audio.addEventListener('canplaythrough', () => {
       console.log('canplaythrough');
       this.setState({buffer: 90});
       console.warn('this.props.player.isPlaying',this.props.player.isPlaying);
       if (this.props.player.isPlaying) {
-        $audio.play();
+        this.$audio.play();
       }
     });
 
 
-    $audio.addEventListener('progress', () => {
+    this.$audio.addEventListener('progress', () => {
       // some music is playing, the user has sound in his ears
       // if disconnected, may be because playing music in buffer
       // console.log('progress', this.state.buffer, e);
 
       if(this.state.buffer < 99) this.setState({buffer: this.state.buffer + 1});
     });
-    $audio.addEventListener('stalled', () => {
+    this.$audio.addEventListener('stalled', () => {
       console.log('stalled');
       this.setState({buffer: 10});
     });
-    $audio.addEventListener('suspend', () => {
+    this.$audio.addEventListener('suspend', () => {
       console.log('suspend');
       this.setState({buffer: 5});
     });
@@ -96,7 +104,8 @@ class Player extends Component {
             defaultValue="90"/>
         </div>
         <div className="display">{ this.props.current.title }</div>
-        <audio controls="controls" src={this.props.current.source} type="audio/mp3" autoPlay={this.props.player.isPlaying} />
+        <audio controls="controls" src={this.props.current.source}
+          type="audio/mp3" autoPlay={this.props.player.isPlaying} />
       </div>
     );
   }
@@ -105,7 +114,7 @@ class Player extends Component {
 Player.propTypes = {
   player: React.PropTypes.object,
   current: React.PropTypes.object,
-  dispatch: React.PropTypes.fun
+  dispatch: React.PropTypes.func
 };
 Player.defaultProps = {
   current: {id:0, title:'default', soucce:''}
